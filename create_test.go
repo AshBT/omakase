cfc2570583a25b017e6f4b038bfc0e55e7641634
Eric Chu <eytchu@gmail.com>
2015-05-01 00:11:32 -0700

    added test cases for create command

--- create_test.go
+++ create_test.go
@@ -1,5 +1,70 @@
 package omakase
 
-import "testing"
+import (
+  "testing"
+  "text/template"
+  "bytes"
+  "os"
+)
 
-func TestFoo(*testing.T) {}
+func TestGetAssetName(t *testing.T) {
+  e := Expectations(t)
+  e.Expect(getAssetName("templates/README.md")).
+    Equals("README.md")
+}
+
+func TestCreateTemplate(t *testing.T) {
+  data := `# test template {{ ignore these }}
+[[ .DiscoveryURL ]]
+[[ .ClusterName ]]
+[[ .PublicKeyPath ]]
+[[ .CloudConfigPath ]]`
+  expected := `# test template {{ ignore these }}
+foobar
+bar
+my/fake/home/bar/bar.pub
+my/fake/home/bar/cloud-config`
+
+  buf := new(bytes.Buffer)
+  ctx := TestContext()
+  e := Expectations(t)
+
+  tmpl := createTemplate("doesn't matter")
+  tmpl = template.Must(tmpl.Parse(data))
+  handleTemplate(ctx, tmpl, buf)
+
+  e.Expect(buf.String()).
+    Equals(expected)
+}
+
+func TestCreate(t *testing.T) {
+  ctx := TestContext()
+  Create(ctx)
+
+  defer os.RemoveAll("bar")
+
+  e := Expectations(t)
+  e.Label("README.md").
+    Expect("bar/README.md").
+    FileExists()
+
+  e.Label("cloud-config").
+    Expect("bar/cloud-config").
+    FileExists()
+
+  e.Label("bar.tf").
+    Expect("bar/bar.tf").
+    FileExists()
+
+  e.Label("variables.tf").
+    Expect("bar/variables.tf").
+    FileExists()
+
+  e.Label("Public key: bar.pub").
+    Expect("bar/bar.pub").
+    FileExists()
+
+  e.Label("Private key: bar").
+    Expect("bar/bar").
+    FileExists()
+}
