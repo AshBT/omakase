291c683a517f315b08c43ae302d271b0eca4895b
Eric Chu <eytchu@gmail.com>
2015-05-01 00:10:20 -0700

    refactored context to take working dir as an argument; simplifies test

--- context.go
+++ context.go
@@ -9,19 +9,21 @@ type Context struct {
   // capitalized members are accessible in templates
   DiscoveryURL    string    // discovery url for this context
   ClusterName     string    // the cluster name
-  PublicKeyPath   string    // path to the public key for ssh access
-  CloudConfigPath string    // path to generated cloud-config file    
+  PublicKeyPath   string    // absolute path to the public key for ssh
+  CloudConfigPath string    // absolute path to generated cloud-config
 }
 
-func NewContext(e Discovery, name string) (*Context) {
-  cwd, err := os.Getwd()
-  check(err)
+func NewContext(e Discovery, name string, workingdir string) (*Context) {
   // Generic context creator
-  return &Context{e.discover(), name, "foo", fmt.Sprintf("%s/%s/cloud-config", cwd, name)}
+  cloudConfig := fmt.Sprintf("%s/%s/cloud-config", workingdir, name)
+  pubkeyPath := fmt.Sprintf("%s/%s/%s.pub", workingdir, name, name)
+  return &Context{e.discover(), name, pubkeyPath, cloudConfig}
 }
 
 func NewEtcdContext(name string) (*Context) {
   // Context creator for default etcd discovery
   etcd := &Etcd {"https://discovery.etcd.io"}
-  return NewContext(etcd, name)
+  cwd, err := os.Getwd()
+  check(err)
+  return NewContext(etcd, name, cwd)
 }
