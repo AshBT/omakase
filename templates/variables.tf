837a7d97b8b5becc5886f72274ff09297cdb6ca8
Eric Chu <eytchu@gmail.com>
2015-05-12 10:16:56 -0700

    added aws keys as variables in terraform, so we can use it in templates

--- templates/variables.tf
+++ templates/variables.tf
@@ -1,16 +1,26 @@
 variable "aws_region" {
-    description = "The AWS region to create things in."
-    default = "us-west-1"
+  description = "The AWS region to create things in."
+  default = "us-west-1"
+}
+
+variable "aws_access_key" {
+  description = "AWS access key"
+  default = ""
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  default = ""
 }
 
 # CoreOS Stable Channel
 variable "aws_amis" {
-    description = "AMIs for CoreOS stable channel"
-    default = {
-        us-west-1 = "ami-43f91b07"
-        us-west-2 = "ami-37280207"
-        us-east-1 = "ami-d2033bba"
-    }
+  description = "AMIs for CoreOS stable channel"
+  default = {
+      us-west-1 = "ami-43f91b07"
+      us-west-2 = "ami-37280207"
+      us-east-1 = "ami-d2033bba"
+  }
 }
 
 variable "count" {
